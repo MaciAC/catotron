c1128a31e96e74511790e5e968d20deee1536bc7
gullabi <bkulebi@gmail.com>
2019-11-11 16:53:27 +0100

    hparams changed for adaptation

--- hparams.py
+++ hparams.py
@@ -10,7 +10,7 @@ def create_hparams(hparams_string=None, verbose=False):
         # Experiment Parameters        #
         ################################
         epochs=500,
-        iters_per_checkpoint=1000,
+        iters_per_checkpoint=250,
         seed=1234,
         dynamic_loss_scaling=True,
         fp16_run=False,
@@ -78,7 +78,7 @@ def create_hparams(hparams_string=None, verbose=False):
         # Optimization Hyperparameters #
         ################################
         use_saved_learning_rate=False,
-        learning_rate=1e-3,
+        learning_rate=4e-4,
         weight_decay=1e-6,
         grad_clip_thresh=1.0,
         batch_size=64,
