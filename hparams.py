c33e521bf428799d53ae9e08f277e96da36939d9
MaciAC <macia.ac@gmail.com>
2022-02-08 16:53:58 +0100

    valencià test

--- hparams.py
+++ hparams.py
@@ -25,8 +25,8 @@ def create_hparams(hparams_string=None, verbose=False):
         # Data Parameters             #
         ################################
         load_mel_from_disk=False,
-        training_files='filelists/upc_pau_train.txt',
-        validation_files='filelists/upc_pau_val.txt',
+        training_files='filelists/valencia_train.txt',
+        validation_files='filelists/valencia_val.txt',
         text_cleaners=['catalan_cleaners'],
 
         ################################
